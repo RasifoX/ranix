ee15b20043bb69e991aae9aba9811054dbb653b7
RasifoX <rasifox34@gmail.com>
2026-01-14 00:06:18 +0300

    arch/x86: fix keyboard shift logic and modular keymap support

--- src/arch/x86/drivers/keyboard.c
+++ src/arch/x86/drivers/keyboard.c
@@ -1,10 +1,14 @@
 #include <stdint.h>
+#include <stdbool.h>
 #include "io.h"
 #include "hal.h"
 #include "keyboard.h"
 #include "keymap.h"
 
 keymap_t *current_layout = 0;
+
+bool shift_pressed = 0;
+bool altgr_pressed = 0;
 
 void keyboard_set_map(keymap_t *new_map)
 {
@@ -15,12 +19,35 @@ void keyboard_handler()
 {
     uint8_t scancode = inb(0x60);
 
+    if (scancode == 0x36 || scancode == 0x2A)
+    {
+        shift_pressed = 1;
+    }
+    else if (scancode == 0xAA || scancode == 0xB6)
+    {
+        shift_pressed = 0;
+    }
+
     if (scancode & 0x80)
         return;
 
     if (current_layout && scancode < 128)
     {
-        char c = current_layout->map[scancode];
+        char c;
+
+        if (shift_pressed)
+        {
+            c = current_layout->map[scancode][1];
+        }
+        else if (altgr_pressed)
+        {
+            c = current_layout->map[scancode][2];
+        }
+        else
+        {
+            c = current_layout->map[scancode][0];
+        }
+
         if (c)
         {
             char str[2] = {c, '\0'};
--- src/arch/x86/include/keymap.h
+++ src/arch/x86/include/keymap.h
@@ -1,10 +1,12 @@
 #ifndef KEYMAP_H
 #define KEYMAP_H
 
-typedef struct
-{
+typedef struct {
     char name[16];
-    unsigned char map[128];
+    
+    // 128 tuş, her tuş için 3 durum:
+    // [0]: Normal, [1]: Shift, [2]: AltGr
+    unsigned char map[128][3]; 
 } keymap_t;
 
 void keyboard_set_map(keymap_t *new_map);
